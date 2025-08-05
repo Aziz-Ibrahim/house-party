3363a6b29438add2f9f6c9bafe1d5947ff294333
Aziz-Ibrahim <dsnxc622@students.codeinstitute.net>
2025-08-05 19:59:40 +0100

    feat; Created a post request to create a room and built a simple UI to manually test

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -10,7 +10,6 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                <h1>House Party App</h1>
                 <HomePage />
             </div>
         );
