9d2f77018072a59bc92a3cdd73119b512382f586
Aziz-Ibrahim <dsnxc622@students.codeinstitute.net>
2025-08-05 22:02:59 +0100

    feat; Added a new component allowing navigations to rooms, added url for it and imported it in home page

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,7 +3,7 @@ from .views import index
 
 urlpatterns = [
     path('', index),  # Main endpoint for the frontend
-    path('join', index),  # Join endpoint for the frontend
+    path('join', index),  # Join endpoint for a specific party
     path('create', index),  # Create endpoint for the frontend
-    path('join/1', index),  # Join endpoint for a specific party
+    path('room/<str:roomCode>', index),  # Room endpoint for a specific room
 ]
