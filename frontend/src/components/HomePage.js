ad51a8604f8e462072751be20223c0b4388dc563
Aziz-Ibrahim <dsnxc622@students.codeinstitute.net>
2025-08-05 15:55:25 +0100

    refractor; Fixed a problem with CLI version that caused components not to render

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -10,17 +10,13 @@ import {
 } from "react-router-dom";
 
 export default class HomePage extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
             <Router>
                 <Routes>
-                    <Route exact path="/"></Route>
-                    <Route path="/join" component={RoomJoinPage} />
-                    <Route path="/create" component={CreateRoomPage} />
+                    <Route path="/" element={<p>This is the home page</p>} />
+                    <Route path="/join" element={<RoomJoinPage />} />
+                    <Route path="/create" element={<CreateRoomPage />} />
                 </Routes>
             </Router>
         );
