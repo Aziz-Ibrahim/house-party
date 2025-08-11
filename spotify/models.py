a8e7fa2ec013f6379548df9af7992a0448e28477
Aziz-Ibrahim <dsnxc622@students.codeinstitute.net>
2025-08-11 22:20:28 +0100

    build; Added a new model to store votes in DB

--- spotify/models.py
+++ spotify/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from api.models import Room
 
 class SpotifyToken(models.Model):
     """
@@ -11,3 +12,13 @@ class SpotifyToken(models.Model):
     access_token = models.CharField(max_length=150)
     expires_in = models.DateTimeField()
     token_type = models.CharField(max_length=50)
+
+
+class Vote(models.Model):
+    """
+    Model to store votes to skip a songs in a room.
+    """
+    user = models.CharField(max_length=50 , unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    song_id = models.CharField(max_length=50)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
